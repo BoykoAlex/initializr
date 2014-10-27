c04c7f2c95bd99b55e2b995ba0bbd05dfb75e9c5
Kris De Volder <kdevolder@gopivotal.com>
2014-10-27 13:54:09 -0700

    Cleanup: comments

--- initializr/src/main/groovy/io/spring/initializr/flux/FluxConnectionFactory.java
+++ initializr/src/main/groovy/io/spring/initializr/flux/FluxConnectionFactory.java
@@ -8,6 +8,10 @@ public class FluxConnectionFactory {
 	
 	public static MessageConnector create(String host, String username, String password) throws Exception {
 		//return FluxClient.DEFAULT_INSTANCE.connect(new SocketIOFluxConfig(host, username, password));
+		
+		//Note: not using 
+		//  - host: because this is the socketio webserver we are connecting directly to rabbitmq
+		//  - password: direct rabbitmq connection requires no password (other than credentials included in rabbitURL)
 		return FluxClient.DEFAULT_INSTANCE.connect(new RabbitMQFluxConfig(username)
 			.setUri(RabbitMQFluxConfig.rabbitUrl()));
 	}
