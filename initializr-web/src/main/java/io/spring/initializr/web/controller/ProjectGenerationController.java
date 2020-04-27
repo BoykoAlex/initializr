73344029c8559c3c96d12bce8ff6b544e86b762f
BoykoAlex <aboyko@pivotal.io>
2020-04-27 19:43:24 -0400

    Polish

--- initializr-web/src/main/java/io/spring/initializr/web/controller/ProjectGenerationController.java
+++ initializr-web/src/main/java/io/spring/initializr/web/controller/ProjectGenerationController.java
@@ -30,15 +30,6 @@ import java.util.function.Function;
 
 import javax.servlet.http.HttpServletResponse;
 
-import io.spring.initializr.generator.buildsystem.BuildSystem;
-import io.spring.initializr.generator.buildsystem.maven.MavenBuildSystem;
-import io.spring.initializr.generator.project.ProjectDescription;
-import io.spring.initializr.metadata.InitializrMetadata;
-import io.spring.initializr.metadata.InitializrMetadataProvider;
-import io.spring.initializr.web.project.InvalidProjectRequestException;
-import io.spring.initializr.web.project.ProjectGenerationInvoker;
-import io.spring.initializr.web.project.ProjectGenerationResult;
-import io.spring.initializr.web.project.ProjectRequest;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
@@ -49,7 +40,6 @@ import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -60,6 +50,16 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.spring.initializr.generator.buildsystem.BuildSystem;
+import io.spring.initializr.generator.buildsystem.maven.MavenBuildSystem;
+import io.spring.initializr.generator.project.ProjectDescription;
+import io.spring.initializr.metadata.InitializrMetadata;
+import io.spring.initializr.metadata.InitializrMetadataProvider;
+import io.spring.initializr.web.project.InvalidProjectRequestException;
+import io.spring.initializr.web.project.ProjectGenerationInvoker;
+import io.spring.initializr.web.project.ProjectGenerationResult;
+import io.spring.initializr.web.project.ProjectRequest;
 
 /**
  * Base {@link Controller} that provides endpoints for project generation.
@@ -126,8 +126,10 @@ public abstract class ProjectGenerationController<R extends ProjectRequest> {
 	}
 
 	@RequestMapping("/generate")
-	public void generate(R request) {
-		this.projectGenerationInvoker.invokeProjectStructureGeneration(request);
+	@ResponseBody
+	public String generate(R request) {
+		ProjectGenerationResult generation = this.projectGenerationInvoker.invokeProjectStructureGeneration(request);
+		return generation.getRootDirectory().toString();
 	}
 
 	@RequestMapping("/starter.zip")
